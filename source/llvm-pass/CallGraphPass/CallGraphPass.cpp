bdfd7c5446f943c4ecc4ce7850f1a12106088f6d
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-09-26 22:28:20 +0530

    [continue] recursive edge add

--- source/llvm-pass/CallGraphPass/CallGraphPass.cpp
+++ source/llvm-pass/CallGraphPass/CallGraphPass.cpp
@@ -20,30 +20,43 @@ namespace {
   struct CallGraphPass : public FunctionPass {
     static char ID;
     uint64_t gID = 0;
-    std::map<void*, std::pair<uint64_t, uint64_t>> blockStartEndState;
-    std::map<std::string, std::map<std::string, std::vector<std::pair<std::string, std::string>>>> graphs;
+    std::map<void*, uint64_t> blockIds;
+    std::map<std::string, std::vector<std::pair<std::string, std::string>>> graph;
+    std::map<std::string, uint64_t> dOut;
+    std::map<std::string, uint64_t> dIn;
 
     CallGraphPass() : FunctionPass(ID) {}
 
-    
-
     virtual bool runOnFunction(Function &Func) {
+      graph.clear();
+      dOut.clear();
+      dIn.clear();
       std::ofstream outfile("graph.txt", std::ios_base::app);
-      uint64_t gID = 0;
+      uint64_t gID = 1;
+
       for (auto &basicBlock : Func) {
         void *blockAddress = static_cast<void*>(&basicBlock);
-        blockStartEndState.insert({blockAddress, {gID++, -1}});
+        blockIds.insert({blockAddress, gID++});
       }
-      errs() << "\ndigraph " << Func.getName() << " {" << "\n";
+      std::vector<uint64_t> funcReturn_vec;
+      
       for(auto &basicBlock : Func){
-        void *blockAddress = static_cast<void*>(&basicBlock);
-        std::pair<uint64_t, uint64_t> nodes = blockStartEndState[blockAddress];
-        processBlockToGraph(nodes, basicBlock, Func, gID, outfile);
+        processBlockToGraph(basicBlock, Func, gID, outfile, funcReturn_vec);
         if (Instruction *terminator = basicBlock.getTerminator()) {
           unsigned numSuccessors = terminator->getNumSuccessors();
           for (unsigned i = 0; i < numSuccessors; ++i) {
-            printEdge(nodes.second, blockStartEndState[terminator->getSuccessor(i)].first, "e", Func, outfile);
+            addEdgeToGraph(gID-1, blockIds[terminator->getSuccessor(i)], "e", Func, outfile);
           }
+        }
+      }
+
+      addStartAndEndNode(funcReturn_vec, Func, gID);
+
+      errs() << "\ndigraph " << Func.getName() << " {" << "\n";
+      for(auto n : graph){
+        for(auto e : n.second){
+          outfile << Func.getName().str() << "," <<  n.first << "," << e.first << "," << e.second  << "\n";
+          errs() << "    " <<  n.first << " -> " << e.first << "[label=\"" << e.second  << "\"];\n";
         }
       }
       errs() << "}" << "\n";
@@ -52,34 +65,57 @@ namespace {
     }
 
 
-    void processBlockToGraph(std::pair<unsigned long int, unsigned long int> &nodes, BasicBlock &basicBlock, Function &func, uint64_t &gID, std::ofstream &file){
-        unsigned long int start = nodes.first;
-        unsigned long int end = nodes.first;
-        for (auto &instruction : basicBlock) {
-          CallInst *callInst = dyn_cast<CallInst>(&instruction);
-          if (callInst == nullptr) continue;
-          Function *calledFunction = callInst->getCalledFunction();
-          if (calledFunction == nullptr) continue;
+    void processBlockToGraph(BasicBlock &basicBlock, Function &func, uint64_t &gID, std::ofstream &file, std::vector<uint64_t> &funcReturn_vec){
+      uint64_t start = blockIds[static_cast<void*>(&basicBlock)];
+      uint64_t end = start;
+      for (auto &instruction : basicBlock) {
+        CallInst *callInst = dyn_cast<CallInst>(&instruction);
+        if (callInst == nullptr) continue;
+        Function *calledFunction = callInst->getCalledFunction();
+        if (calledFunction == nullptr) continue;
 
+        if(calledFunction->getName().str() == func.getName().str()){
           start = end;
           end = gID++;
-          printEdge(start, end, calledFunction->getName().str(), func, file);
-          if(calledFunction->getName().str() == func.getName().str()){
-            printEdge(end, 0, "e", func, file);
+          funcReturn_vec.push_back(end);
+          addEdgeToGraph(start, blockIds[static_cast<void*>(&func.getEntryBlock())], "call_" + calledFunction->getName().str(),func, file);
+        } else{
+          start = end;
+          end = gID++;
+          addEdgeToGraph(start, end, calledFunction->getName().str(), func, file);
+        }
+      }
+    }
+
+    void addStartAndEndNode(std::vector<uint64_t> funcReturn_vec, Function &func, uint64_t &gID){
+      uint64_t valueToFind = 0;
+      uint64_t entry_node =  blockIds[static_cast<void*>(&func.getEntryBlock())];
+      
+      graph[getNodeName(0, func)].push_back({getNodeName(entry_node, func), "e"});
+      std::string endNode = getNodeName(gID++, func);
+
+      for(auto item : dOut){
+        if(item.second == 0){
+          graph[item.first].push_back({endNode, "e"});
+          for(auto ret : funcReturn_vec){
+            graph[item.first].push_back({getNodeName(ret, func), "e"});
           }
         }
-        nodes.second = end;
+      }
     }
 
     std::string getNodeName(uint64_t nodeId, Function &func){
       return func.getName().str() + "_" + std::to_string(nodeId);
     }
 
-    void printEdge(uint64_t src, uint64_t dst, std::string e, Function &func, std::ofstream &file){
+    void addEdgeToGraph(uint64_t src, uint64_t dst, std::string e, Function &func, std::ofstream &file){
       std::string start = getNodeName(src, func);
       std::string end = getNodeName(dst, func);
-      file << func.getName().str() << "," << start << "," << end << "," << e << "\n";
-      errs() << "    " << start << " -> " << end << "[label=\"" << e << "\"];\n";
+      dOut[start]++;
+      dIn[end]++;
+      if(dOut.find(end) == dOut.end()) dOut[end] = 0;
+      if(dIn.find(start) == dIn.end()) dOut[start] = 0;
+      graph[start].push_back({end, e});
     }
   };
 }
