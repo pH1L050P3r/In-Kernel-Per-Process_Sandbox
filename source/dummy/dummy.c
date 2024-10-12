bdded184030ae4def9b5a8018748b9d1b52eff0f
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-10-12 17:13:04 +0530

    update dummy.c to print \n after every print

--- source/dummy/dummy.c
+++ source/dummy/dummy.c
@@ -1,6 +1,8 @@
 #include <sys/syscall.h>
+#include <stdio.h>
 
 int dummy(int code){
     int sys_call_number_for_dummy = 1024;
+    printf("function Called with ID : %d\n", code);
     return syscall(sys_call_number_for_dummy, code);
 }
