0a5336b936837869515f3e398ce7bc0a965df15f
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-10-13 11:36:43 +0530

    Fix Add dumy syscall add before library call

--- source/llvm-pass/DummyCallAddPass/DummyCallAddPass.cpp
+++ source/llvm-pass/DummyCallAddPass/DummyCallAddPass.cpp
@@ -52,7 +52,7 @@ namespace {
       for (auto &BB : Func) {
           for (auto &I : BB) {
               auto *callInst = dyn_cast<CallInst>(&I);
-              if (callInst && libc_func.find(callInst->getCalledFunction()->getName().str()) != libc_func.end()) {
+              if (callInst && callInst->getCalledFunction() && libc_func.find(callInst->getCalledFunction()->getName().str()) != libc_func.end()) {
                   errs() << "I am here " << callInst->getCalledFunction()->getName().str() << "\n"; 
                   builder.SetInsertPoint(&BB, std::next(BB.getFirstInsertionPt()));
                   Value *value = builder.getInt32(libc_func[callInst->getCalledFunction()->getName().str()]);
