d0d8d535b855cea391db4d0c621e01ac78e1e1dc
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-11-17 15:34:49 +0530

    Modified Dummy call add pass to generate library function call list also

--- source/llvm-pass/DummyCallAddPass/DummyCallAddPass.cpp
+++ source/llvm-pass/DummyCallAddPass/DummyCallAddPass.cpp
@@ -25,8 +25,9 @@ using namespace llvm;
 namespace {
   struct DummyCallAddPass : public FunctionPass {
     static char ID;
-    const std::string libc_function = std::string(__FILE__).substr(0, std::string(__FILE__).find_last_of("/\\")) + "/musl_functions.txt";
-    std::map<std::string, uint32_t> libc_func; 
+    const std::string libc_function = std::string(__FILE__).substr(0, std::string(__FILE__).find_last_of("/\\")) + "/library_functions.txt";
+    std::map<std::string, uint32_t> libc_func;
+    std::set<std::string> fname; 
 
     DummyCallAddPass() : FunctionPass(ID) {
       std::ifstream inFile(libc_function);
@@ -43,6 +44,19 @@ namespace {
       inFile.close();
     }
 
+    virtual bool doInitialization(Module &)  {
+      fname.clear();
+      deserializeSet("library_function_list.txt"); 
+      return false; 
+    }
+
+    virtual bool doFinalization(Module &) { 
+      serializeSet("library_function_list.txt");
+      return false; 
+    }
+
+
+
     bool runOnFunction(Function &Func){
       errs() << libc_func.size() << "\n";
       LLVMContext &context = Func.getParent()->getContext();
@@ -58,10 +72,35 @@ namespace {
                   builder.SetInsertPoint(callInst);
                   builder.CreateCall(dummySyscallFunc, {value});
                   values = callInst->getCalledFunction()->getName().str() + " return \n";
+                  fname.insert(callInst->getCalledFunction()->getName().str());
               }
           }
       }
       return true;
+    }
+
+    void serializeSet(const std::string& filename) {
+      std::ofstream outFile(filename, std::ios::trunc);
+      if (!outFile) {
+          errs() << "Could not open the file for writing: " << filename << "\n";
+          return;
+      }
+      for (const std::string& element : fname)
+          outFile << element <<  "\n"; // Write each element to a new line
+      outFile.close();
+    }
+
+    void deserializeSet(const std::string& filename) {
+      std::ifstream inFile(filename);
+      if (!inFile) {
+          errs() << "Could not open the file for reading: " << filename << "\n";
+          return;
+      }
+
+      std::string element;
+      while (inFile >> element)
+          fname.insert(element); // Insert each element into the set
+      inFile.close();
     }
   };
 }
