4fe5e1786a4a0c7b214f70fa0149432d0fde2772
Siddharth Baudh <siddharthbaudhya6216@gmail.com>
2024-11-22 22:46:03 +0530

    increase BCC_PROBE_LIMIT to 5000

--- source/eBPF/run.sh
+++ source/eBPF/run.sh
@@ -1,1 +1,3 @@
+ulimit -u 65000
+export BCC_PROBE_LIMIT=50000
 python3 enforce_NFA_ebpf.py --dot-file ./graph.dot --function-map ./library_functions.txt --library-functions ./called_lib_functions.txt
